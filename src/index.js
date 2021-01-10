2f104f9c0cbbd845f935c948a7d030b03ffa5525
Carlos Quadros <carlosalbertoquadros@gmail.com>
2021-01-10 12:13:26 -0300

    modal issues and contributors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 var cards = document.querySelector('#cards')
 
+function getRepoName() {
+    return repoName = repository['name'].replace(/-/g, " ")
+}
+
+//===============modal creation =============
+function createModal(repository) {
+    console.log(repository);
+    getRepoName()
+    document.querySelector('.modal-title').textContent = repoName;
+
+    let contributorsURL = repository['contributors_url']
+    console.log(contributorsURL);
+
+    let issuesUrl = repository['issues_url']
+    console.log(issuesUrl);
+/////////////////////////////////////////////////////parei aqui buscando issues url
+}
+///=========card creation==============
 function createCard(repository) {
+    //get url
+    let repoUrl = repository["url"]
     //get owner img
     let owner = repository['owner'];
     let ownerAvatar = owner['avatar_url']
 
     //get repository name
-    let repoName = repository['name'].replace(/-/g, " ")
+    getRepoName()
 
     //get repository description
     let repoDescription = repository['description'];
@@ -15,20 +35,27 @@ function createCard(repository) {
     let div = document.createElement("div")
     div.classList.add("col")
     div.innerHTML =
-        `<div class="card h-100">
+        `
+        <div class="card fade-in-right h-100">
         <img src="${ownerAvatar}" class="card-img-top " alt="...">
         <div class="card-body">
-            <h5 class="card-title fw-bolder text-center">${repoName}</h5>
+            <h5 class="card-title fw-bolder text-center text-capitalize">${repoName}</h5>
             <p class="card-text">${repoDescription}</p>
+            <a onclick="modalRequest(this.id)" id="${repoUrl}" href="" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#modal">Go somewhere</a>
         </div>
-    </div>`
+    </div></a>`
     cards.appendChild(div)
+
 }
 
 
 
-function repositoryRequest(apiLink) {
-    //campos a serem preenchidos
+
+
+//======================================================
+
+function modalRequest(apiLink) {  /// setFuntion options:  createCard  ||  createModal ///
+    //requisição api github: https://developer.github.com/v3
     let ajax = new XMLHttpRequest();
     ajax.open("get", apiLink)
     ajax.send(null)
@@ -37,18 +64,46 @@ function repositoryRequest(apiLink) {
             if (ajax.status === 200) {
                 //transformar os dados json para array
                 repository = JSON.parse(ajax.responseText);
-                createCard(repository)
-
+                createModal(repository)
             } else {
-                alert("deu merda")
-
+                alert("Repository not found")
             }
         }
     }
 }
-repositoryRequest('https://api.github.com/repos/filipedeschamps/meu-primeiro-jogo-multiplayer')
-repositoryRequest('https://api.github.com/repos/facebook/react-native')
-repositoryRequest('https://api.github.com/repos/twbs/bootstrap')
+
+
+
+
+
+
+
+//=============================================================================
+function repositoryRequest(apiLink, setFuntion) {  /// setFuntion options:  createCard  ||  createModal ///
+    //requisição api github: https://developer.github.com/v3
+    let ajax = new XMLHttpRequest();
+    ajax.open("get", apiLink)
+    ajax.send(null)
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+            if (ajax.status === 200) {
+                //transformar os dados json para array
+                repository = JSON.parse(ajax.responseText);
+
+                setFuntion(repository)
+            } else {
+                alert("Repository not found")
+            }
+        }
+    }
+}
+repositoryRequest('https://api.github.com/repos/filipedeschamps/meu-primeiro-jogo-multiplayer', createCard)
+repositoryRequest('https://api.github.com/repos/facebook/react-native', createCard)
+repositoryRequest('https://api.github.com/repos/twbs/bootstrap', createCard)
+
+//===========modal=============/
+
+
 
 
 
