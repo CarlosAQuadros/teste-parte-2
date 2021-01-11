ce01212727403c40f9a38530fff6e849ed1b22aa
Carlos Quadros <carlosalbertoquadros@gmail.com>
2021-01-10 21:17:22 -0300

    finished lists  template

--- src/index.js
+++ src/index.js
@@ -1,23 +1,70 @@
 var cards = document.querySelector('#cards')
+var tableContributors = document.querySelector("#tbodyContributors");
+var tableIssues = document.querySelector("#tbodyIssues");
+
+function cleanTbody() {
+    tableContributors.innerHTML = "";
+    tableIssues.innerHTML = ";"
+}
 
 function getRepoName() {
     return repoName = repository['name'].replace(/-/g, " ")
 }
 
+//=========create contributors list
+
+function createContributorsList(repository) {
+    //console.log(repository);
+    for (contributor of repository) {
+        let tbody = document.createElement('tr')
+
+        tbody.innerHTML = `
+        <th scope="row"><img src="${contributor['avatar_url']}" alt="" width="32" height="32" " alt="..."></th>
+       <td>${contributor['contributions']}</td>
+       <td>${contributor['login']}</td>`;
+        tableContributors.appendChild(tbody)
+    }
+}
+
+function createIssuesList(repository) {
+    console.log(repository);
+    for (issue of repository) {
+
+        console.log(issue);
+
+        let tbody = document.createElement('tr')
+
+
+        tbody.innerHTML = ` 
+            <th scope="row">#</th>
+         <td>${issue['title']}</td>
+           <td>${issue['state']}</td>`;
+        tableIssues.appendChild(tbody)
+    }
+
+}
+
 //===============modal creation =============
 function createModal(repository) {
-    console.log(repository);
+    //console.log(repository);
     getRepoName()
+
+    let repoUrl = repository["url"]
+    let contributorsURL = repoUrl + '/contributors';
+    //console.log("contributors ==" + contributorsURL);
+    repositoryRequest(contributorsURL, createContributorsList);
+
+    //
+
+    let issuesUrl = repoUrl + '/issues?state=all';
+    repositoryRequest(issuesUrl, createIssuesList)
+
     document.querySelector('.modal-title').textContent = repoName;
 
-    let contributorsURL = repository['contributors_url']
-    console.log(contributorsURL);
-
-    let issuesUrl = repository['issues_url']
-    console.log(issuesUrl);
-/////////////////////////////////////////////////////parei aqui buscando issues url
 }
+
 ///=========card creation==============
+
 function createCard(repository) {
     //get url
     let repoUrl = repository["url"]
@@ -41,42 +88,12 @@ function createCard(repository) {
         <div class="card-body">
             <h5 class="card-title fw-bolder text-center text-capitalize">${repoName}</h5>
             <p class="card-text">${repoDescription}</p>
-            <a onclick="modalRequest(this.id)" id="${repoUrl}" href="" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#modal">Go somewhere</a>
+            <a onclick="repositoryRequest(this.id, createModal)" id="${repoUrl}" href="" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#modal">Go somewhere</a>
         </div>
     </div></a>`
     cards.appendChild(div)
 
 }
-
-
-
-
-
-//======================================================
-
-function modalRequest(apiLink) {  /// setFuntion options:  createCard  ||  createModal ///
-    //requisição api github: https://developer.github.com/v3
-    let ajax = new XMLHttpRequest();
-    ajax.open("get", apiLink)
-    ajax.send(null)
-    ajax.onreadystatechange = function () {
-        if (ajax.readyState === 4) {
-            if (ajax.status === 200) {
-                //transformar os dados json para array
-                repository = JSON.parse(ajax.responseText);
-                createModal(repository)
-            } else {
-                alert("Repository not found")
-            }
-        }
-    }
-}
-
-
-
-
-
-
 
 //=============================================================================
 function repositoryRequest(apiLink, setFuntion) {  /// setFuntion options:  createCard  ||  createModal ///
@@ -102,10 +119,6 @@ repositoryRequest('https://api.github.com/repos/facebook/react-native', createCa
 repositoryRequest('https://api.github.com/repos/twbs/bootstrap', createCard)
 
 //===========modal=============/
-
-
-
-
 
 
 
